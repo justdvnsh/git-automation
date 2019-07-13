8e6493cc8db8c31354b55ca4cd264030a0ef9690
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2019-07-13 23:55:48 +0530

    again something from the script

--- create.sh
+++ create.sh
@@ -6,10 +6,13 @@ STRING="Hello World!"
 echo $STRING
 
 read -r -p 'Commit message: ' desc  # prompt user for commit message
+git status
 git add .                           # track all files
 git add -u                          # track deletes
 git commit -m "$desc"               # commit with message
 git push origin master              # push to origin
+
+python hello.py
 
 function create() {
     cd ~/Desktop/
